251e95ac848848dff43586f30a66e2339114b425
AsimSafarli <asimseferli014@gmail.com>
2023-07-11 17:52:37 +0400

    modified layout.jsx and navbar.jsx , new file home.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-
+import { Link, NavLink } from 'react-router-dom'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {GrFavorite} from 'react-icons/gr'
 function Navbar() {
   return (
-    <div>
-     <header>
-        <div><img src='Assets/Logo.png' className='w-16'/></div>
-        <nav className='font-bold'>
+    <div className='p-4 '>
+     <header className='flex items-center justify-between'>
+        <div><Link to='/'><img src='Assets/Logo.png' className='w-16'/></Link></div>
+        <nav className='font-bold flex flex-row gap-x-4 text-[#224453]'>
             <NavLink to='/' >AnaSeyfe </NavLink>
             <NavLink to='/categories' >Kateqoriya </NavLink>
             <NavLink to='/win' >Qazananlar</NavLink>
@@ -14,7 +15,12 @@ function Navbar() {
             <NavLink to='/faq' >Suallar</NavLink>
             <NavLink to='/contact'>Elaqe</NavLink>
         </nav>
-        <div></div>
+        <div className='flex flex-row items-center gap-x-7 font-bold '>
+            <div><GrFavorite className='text-2xl'/></div>
+            <div><AiOutlineShoppingCart className='text-2xl'/></div>
+            <div><NavLink to='/login' className='bg-[#224453] p-2 rounded-md text-white hover:text-[#224453] hover:border hover:border-[#224453] hover:bg-transparent'>Login</NavLink></div>
+            <div><NavLink to='/login' className='bg-[#ED5C01] p-2 rounded-md text-white hover:text-[#ED5C01] hover:border hover:border-[#ED5C01] hover:bg-transparent'>Register</NavLink></div>
+        </div>
      </header>
      </div>
   )
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from '../Components/Footer/Footer'
+import Home from '../Page/Home/Home';
 
 function Layout() {
+  const location = useLocation();  
+
   return (
     <div>
     <Navbar/>
+    {location.pathname === "/" && <Home/>} 
     <Outlet/>
     <Footer/>
     </div>

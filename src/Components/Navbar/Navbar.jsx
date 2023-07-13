898dd98b02a9ce361cbeb733c1aacbe6cba9c10f
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-07-13 23:48:09 +0400

    navbar and 2 section finished

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import {AiOutlineShoppingCart} from 'react-icons/ai'
-import {GrFavorite} from 'react-icons/gr'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsSearch } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
+import "./Navbar.css";
 function Navbar() {
   return (
-    <div className='p-4 '>
-     <header className='flex items-center justify-between'>
-        <div><Link to='/'><img src='Assets/Logo.png' className='w-16'/></Link></div>
-        <nav className='font-bold flex flex-row gap-x-4 text-[#224453]'>
-            <NavLink to='/' >AnaSeyfe </NavLink>
-            <NavLink to='/categories' >Kateqoriya </NavLink>
-            <NavLink to='/win' >Qazananlar</NavLink>
-            <NavLink to='/about' >Haqqimizda</NavLink>
-            <NavLink to='/faq' >Suallar</NavLink>
-            <NavLink to='/contact'>Elaqe</NavLink>
-        </nav>
-        <div className='flex flex-row items-center gap-x-7 font-bold '>
-            <div><GrFavorite className='text-2xl'/></div>
-            <div><AiOutlineShoppingCart className='text-2xl'/></div>
-            <div><NavLink to='/login' className='bg-[#224453] p-2 rounded-md text-white hover:text-[#224453] hover:border hover:border-[#224453] hover:bg-transparent'>Login</NavLink></div>
-            <div><NavLink to='/login' className='bg-[#ED5C01] p-2 rounded-md text-white hover:text-[#ED5C01] hover:border hover:border-[#ED5C01] hover:bg-transparent'>Register</NavLink></div>
+    <div className="p-5">
+      <header className=" header ">
+        <div className="loqo">
+          <Link to="/">
+            <img src="Assets/navbar/Logo.png" />
+          </Link>
+          <h1>Forturaffle</h1>
         </div>
-     </header>
-     </div>
-  )
+        <nav className="nav">
+          <NavLink to="/">Ana səhifə </NavLink>
+          <NavLink to="/categories">Məhsullar</NavLink>
+          <NavLink to="/win"> Son qazananlar</NavLink>
+          <NavLink to="/about">Haqqımızda</NavLink>
+          <NavLink to="/faq">FAQ</NavLink>
+          <NavLink to="/contact">Əlaqə</NavLink>
+        </nav>
+        <div className="navbar__loqo ">
+          <div>
+            <BsSearch className="text-2xl " />
+          </div>
+          <div>
+            <AiOutlineHeart className="text-2xl" />
+          </div>
+          <div>
+            <AiOutlineShoppingCart className="text-2xl" />
+          </div>
+          <div className="text-2xl">
+            <FaRegUserCircle />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;

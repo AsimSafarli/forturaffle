aedfc9b57f72c724cbbf4049dfeb25b83cb280aa
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-07-15 03:49:06 +0400

    Navbar addded active link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
 import { FaRegUserCircle } from "react-icons/fa";
 import "./Navbar.css";
+
 function Navbar() {
+  const [activeLink, setActiveLink] = useState("");
+
+  const handleClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <div className="p-5">
-      <header className=" header ">
+      <header className="header">
         <div className="loqo">
-          <Link to="/">
-            <img src="Assets/navbar/Logo.png" />
+          <Link to="/" onClick={() => setActiveLink("")}>
+            <img src="Assets/navbar/Logo.png" alt="Logo" />
           </Link>
-          <h1>Forturaffle</h1>
+          <h1>
+            <Link to="/" onClick={() => setActiveLink("")}>
+              Forturaffle
+            </Link>
+          </h1>
         </div>
         <nav className="nav">
-          <NavLink to="/">Ana səhifə </NavLink>
-          <NavLink to="/categories">Məhsullar</NavLink>
-          <NavLink to="/win"> Son qazananlar</NavLink>
-          <NavLink to="/about">Haqqımızda</NavLink>
-          <NavLink to="/faq">FAQ</NavLink>
-          <NavLink to="/contact">Əlaqə</NavLink>
+          <NavLink
+            exact
+            to="/"
+            className={`navLink ${activeLink === "" ? "activeLink" : ""}`}
+            onClick={() => handleClick("")}
+          >
+            Ana səhifə
+          </NavLink>
+          <NavLink
+            to="/categories"
+            className={`navLink ${
+              activeLink === "categories" ? "activeLink" : ""
+            }`}
+            onClick={() => handleClick("categories")}
+          >
+            Məhsullar
+          </NavLink>
+          <NavLink
+            to="/win"
+            className={`navLink ${
+              activeLink === "win" ? "activeLink" : ""
+            }`}
+            onClick={() => handleClick("win")}
+          >
+            Son qazananlar
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={`navLink ${activeLink === "about" ? "activeLink" : ""}`}
+            onClick={() => handleClick("about")}
+          >
+            Haqqımızda
+          </NavLink>
+          <NavLink
+            to="/faq"
+            className={`navLink ${activeLink === "faq" ? "activeLink" : ""}`}
+            onClick={() => handleClick("faq")}
+          >
+            FAQ
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={`navLink ${
+              activeLink === "contact" ? "activeLink" : ""
+            }`}
+            onClick={() => handleClick("contact")}
+          >
+            Əlaqə
+          </NavLink>
         </nav>
-        <div className="navbar__loqo ">
+        <div className="navbar__loqo">
           <div>
-            <BsSearch className="text-2xl " />
+            <BsSearch className="text-2xl" />
           </div>
           <div>
             <AiOutlineHeart className="text-2xl" />

f66bf0bae69511ebdbb0a9d576442470196eec2b
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-07-16 04:21:24 +0400

    The data folder has been created and some changes have been made

---
+++ src/Components/HomePage/SansYoxla/SansYoxla.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./SansYoxla.css";
+const SansYoxla = () => {
+  return (
+    <div>
+      <section className="first__section">
+        <div className="first__section__left">
+          <div className="first__section__left__title">
+            <h1>Şansınızı yoxlayın!</h1>
+          </div>
+          <div className="first__section__left__decription">
+            <div className="first__section__left__decription__left">
+              <h6>500+</h6>
+              <p>Müştəri</p>
+            </div>
+
+            <div className="first__section__left__decription__right">
+              <h6>1000+</h6>
+              <p>Məhsul</p>
+            </div>
+          </div>
+          <div className="first__section__left__button">
+            <button className="first__section__left__button__btn">Bileti al</button>
+          </div>
+        </div>
+
+        <div className="first__section__right">
+          <img src="Assets/Home/Shop giveaway-pana 1.png" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SansYoxla;
